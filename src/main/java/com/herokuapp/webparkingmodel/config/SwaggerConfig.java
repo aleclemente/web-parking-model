5c6dafe927921fd5f26291ee3a2826e40a2c8a25
Alexandre Clemente <aleclemente@gmail.com>
2022-08-14 20:43:13 -0300

    Fisrt Swagger configuration

---
+++ src/main/java/com/herokuapp/webparkingmodel/config/SwaggerConfig.java
@@ -0,0 +1,36 @@
+package com.herokuapp.webparkingmodel.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Component
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket getDocket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.herokuapp.webparkingmodel"))
+                .build()
+                .apiInfo(metaData());
+
+    }
+
+    private ApiInfo metaData() {
+        return new ApiInfoBuilder()
+                .title("Web Parking Model")
+                .description("Java project to simulate a web vehicle parking model to host on Heroku cloud application platform.")
+                .version("1.0.0")
+                .license("Apache License Version 2.0")
+                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0\"")
+                .build();
+    }
+
+}
--- src/main/java/com/herokuapp/webparkingmodel/controller/WelcomeController.java
+++ src/main/java/com/herokuapp/webparkingmodel/controller/WelcomeController.java
@@ -3,9 +3,11 @@ package com.herokuapp.webparkingmodel.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
 
 @RestController
 @RequestMapping("/")
+@ApiIgnore
 public class WelcomeController {
     @GetMapping
     public String welcomeMessage(){
